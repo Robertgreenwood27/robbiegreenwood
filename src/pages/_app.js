972ef13360790c7e7cd07324e03abe2e78413400
Robert Greenwood <RobertGreenwood27@gmail.com>
2024-03-07 14:18:06 -0700

    changed og image

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,7 @@ import BackgroundCanvas from '@/components/BackgroundCanvas';
 import { LoadingProvider, useLoading } from '../components/LoadingContext'
 import Footer from '@/components/Footer'
 import { Analytics } from "@vercel/analytics/react"
+import Head from 'next/head';
 
 
 const AppContent = ({ Component, pageProps }) => {
@@ -13,6 +14,17 @@ const AppContent = ({ Component, pageProps }) => {
   return (
     <>
     <Analytics/>
+    <Head>
+  <meta name="description" content="Robbie Greenwood's web developer portfolio showcasing skills and projects." />
+  <link rel="icon" href="/favicon.ico" />
+  
+  {/* Open Graph meta tags */}
+  <meta property="og:title" content="Robbie Greenwood - Web Developer Portfolio" />
+  <meta property="og:description" content="Robbie Greenwood's web developer portfolio showcasing skills and projects." />
+  <meta property="og:image" content="/robbie.png" />
+  <meta property="og:url" content="https://robbiegreenwood.dev" />
+  <meta property="og:type" content="website" />
+</Head>
       <Header />
       <BackgroundCanvas isLoading={isLoading} />
       <Component {...pageProps}/>
